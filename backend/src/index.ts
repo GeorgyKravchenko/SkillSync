96d0136b02048d7c7ac89fdad70a0a7d08bd361a
GeorgyKravchenko <georgykravchenko2025@gmail.com>
2025-05-27 13:38:25 +0300

    feat(backend) add CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,12 +5,18 @@ import dotenv from 'dotenv';
 import authRouter from './routes/auth.route';
 import profileRouter from './routes/profile.route';
 import authMiddleware from './middleware/auth.middleware';
-
+import cors from 'cors';
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || 'http://localhost:3000',
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(helmet());
 app.use('/api/auth', authRouter);
